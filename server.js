6a615dd7055ce0a794d96eedb7c46024faeb12f0
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-12-27 11:30:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
-app.use("/", (req, res) => {
+app.use("/home", (req, res) => {
     res.status(200).send("<h1>Welcome to the product server</h1>")
 })
 
