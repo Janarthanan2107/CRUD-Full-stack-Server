2055e7b8b9ed4e1c7796e1f94ddf1d00d11fe168
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-12-27 11:08:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express"
 import productRouter from "./routes/products.Route.js";
 import userRouter from "./routes/users.Route.js"
 
+import cors from "cors"
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
@@ -12,6 +13,9 @@ const CONNECTION_URL = process.env.CONNECTION_URL;
 
 // use external file
 app.use(express.static("../client"))
+
+// third party application permission
+app.use(cors())
 // middleware for define the json formate
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
@@ -19,6 +23,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use("/", (req, res) => {
     res.status(200).send("<h1>Welcome to the product server</h1>")
 })
+
 // middleware to define the product router with application
 app.use("/api/v1/products", productRouter)
 app.use("/api/v1/users", userRouter);
