fc7f086dd12e9ff2a5ac18379df62fa803a73091
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-12-27 11:05:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.use(express.static("../client"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
+app.use("/", (req, res) => {
+    res.status(200).send("<h1>Welcome to the product server</h1>")
+})
 // middleware to define the product router with application
 app.use("/api/v1/products", productRouter)
 app.use("/api/v1/users", userRouter);
